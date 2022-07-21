b4e16eb24a5e69da8587e1b278c1c9650630e264
José Rogério <joserogerio.net@gmail.com>
2022-07-21 17:42:43 -0300

    feat: Multiple Features

---
+++ src/main/java/com/rogeriodev/alfalogistics/api/model/EntregaModel.java
@@ -0,0 +1,23 @@
+package com.rogeriodev.alfalogistics.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.rogeriodev.alfalogistics.domain.model.StatusEntrega;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaModel {
+
+	private Long id;
+	private ClienteResumoModel cliente;
+	private DestinatarioModel destinatario;
+	private BigDecimal taxa;
+	private StatusEntrega status;
+	private OffsetDateTime dataPedido;
+	private OffsetDateTime dataFinalizacao;
+	
+}
---
+++ src/main/java/com/rogeriodev/alfalogistics/domain/service/FinalizacaoEntregaService.java
@@ -0,0 +1,27 @@
+package com.rogeriodev.alfalogistics.domain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rogeriodev.alfalogistics.domain.model.Entrega;
+import com.rogeriodev.alfalogistics.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class FinalizacaoEntregaService {
+
+	private EntregaRepository entregaRepository;
+	private BuscaEntregaService buscaEntregaService;
+	
+	@Transactional
+	public void finalizar(Long entregaId) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		
+		entrega.finalizar();
+		
+		entregaRepository.save(entrega);
+	}
+	
+}
